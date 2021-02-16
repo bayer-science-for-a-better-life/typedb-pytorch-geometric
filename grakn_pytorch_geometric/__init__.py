d8fc4eebc3684b24644b34e46dd74afb000ca7ee
Joren Retel <joren.retel@bayer.com>
2021-02-16 12:23:17 +0100

    scm versioning.

---
+++ grakn_pytorch_geometric/__init__.py
@@ -0,0 +1,11 @@
+__author__ = ["Joren Retel"]
+__email__ = ["joren.retel@bayer.com"]
+
+
+try:
+    from grakn_pytorch_geometric._version import version as __version__
+except ImportError:
+    # this protects against an edge case where a user tries to import
+    # the module without installing it, by adding it manually to
+    # sys.path or trying to run it directly from the git checkout.
+    __version__ = "not-installed"
--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup
 
 setup(
     name="grakn-pytorch-geometric",
-    version="",
+    use_scm_version={
+        "version_scheme": "post-release",
+        "write_to": "grakn_pytorch_geometric/_version.py",
+    },
+    setup_requires=['setuptools_scm'],
     packages=[
         "grakn_pytorch_geometric",
         "grakn_pytorch_geometric.data",
         "grakn_pytorch_geometric.models",
+        "grakn_pytorch_geometric.utils",
     ],
     url="",
     license="",
